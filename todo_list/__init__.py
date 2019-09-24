7e01d0c1f656e2582a8a53072fcbf3aa3e0d81cf
Petr Ushakov <ko3luhbka@gmail.com>
2019-09-24 23:15:18 +0300

    Add secret key for handling CSRF token

--- todo_list/__init__.py
+++ todo_list/__init__.py
@@ -1,5 +1,12 @@
+import os
+
 from flask import Flask
 
+
 app = Flask(__name__)
+# workaround of 'A secret key is required to use CSRF'error
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+
 import todo_list.api
 import todo_list.views
