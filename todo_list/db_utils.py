ef54c5eef2960719f79282e07b9a5a4dd390b166
Petr Ushakov <ko3luhbka@gmail.com>
2019-09-29 23:13:39 +0300

    Add kind of logging to db_utils

--- todo_list/db_utils.py
+++ todo_list/db_utils.py
@@ -1,11 +1,9 @@
 import os
 import sqlite3
 
+from todo_list.todo_task import TaskStatus
+
 DB_PATH = os.path.join(os.path.dirname(__file__), 'todo.db')
-# ToDo list items statuses
-NOTSTARTED = 'Not Started'
-INPROGRESS = 'In Progress'
-COMPLETED = 'Completed'
 
 
 def connect_to_db(db_path=DB_PATH):
@@ -14,34 +12,39 @@ def connect_to_db(db_path=DB_PATH):
 
 
 def add_item(item):
+    print(f'=== adding item [{item}]')
     try:
-        print('item: ', item)
+        print('=== item: ', item)
         conn = connect_to_db()
         cur = conn.cursor()
         cur.execute(
             'INSERT INTO items(item, status) VALUES(?,?)',
-            (item, NOTSTARTED)
+            (item, TaskStatus.NOTSTARTED.value)
         )
         conn.commit()
-        return {'item': item, 'status': NOTSTARTED}
+        return {'item': item, 'status': TaskStatus.NOTSTARTED.value}
+
     except Exception as e:
         print('Error: ', e)
         return None
 
 
 def get_all_items():
+    print('=== getting all items')
     try:
         conn = connect_to_db()
         cur = conn.cursor()
         cur.execute('select * from items')
         rows = cur.fetchall()
         return {'count: ': len(rows), 'items': rows}
+
     except Exception as e:
-        print('Error: ', e)
+        print('=== Error: ', e)
         return None
 
 
 def get_item(item):
+    print(f'=== getting item [{item}]')
     try:
         conn = connect_to_db()
         cur = conn.cursor()
@@ -49,34 +52,26 @@ def get_item(item):
         status = cur.fetchone()[0]
         return status
     except Exception as e:
-        print('Error: ', e)
+        print('=== Error: ', e)
         return None
 
 
 def update_status(item, status):
-    status_lower = status.lower().strip()
-    if status_lower == 'not started':
-        status = NOTSTARTED
-    elif status_lower == 'in progress':
-        status = INPROGRESS
-    elif status_lower == 'completed':
-        status = COMPLETED
-    else:
-        print("Invalid status: ", status)
-        return None
-
+    print(f'=== updating status of item [{item}] to [{status}]')
     try:
         conn = connect_to_db()
         cur = conn.cursor()
         cur.execute('update items set status=? where item=?', (status, item))
         conn.commit()
         return {item: status}
+    
     except Exception as e:
         print('Error: ', e)
         return None
 
 
 def delete_item(item):
+    print(f'=== deleting item [{item}]')
     try:
         conn = connect_to_db()
         cur = conn.cursor()
@@ -84,5 +79,5 @@ def delete_item(item):
         conn.commit()
         return {'item': item}
     except Exception as e:
-        print('Error: ', e)
+        print('=== Error: ', e)
         return None
