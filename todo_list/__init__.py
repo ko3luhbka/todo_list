b4da1378d5b3167c62ff83bfd746abc4e2c3f2af
Petr Ushakov <ko3luhbka@gmail.com>
2019-11-16 18:17:24 +0300

    Fix wrong imports order

--- todo_list/__init__.py
+++ todo_list/__init__.py
@@ -3,7 +3,6 @@ import os
 
 from flask import Flask
 
-from todo_list import api, views
 from todo_list.helpers import generate_timestamp
 
 logging.basicConfig(
@@ -11,10 +10,10 @@ logging.basicConfig(
     level=logging.DEBUG,
 )
 
-
 app = Flask(__name__)
 # Workaround of "A secret key is required to use CSRF"error
 SECRET_KEY = os.urandom(32)
 TEMPLATES_AUTO_RELOAD = True
 app.config.from_object(__name__)
 app.add_template_global(generate_timestamp, name='timestamp')
+from todo_list import api, views
