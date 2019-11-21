8aebc7fc7ea1ac3ccf9e86529866f91c187afb90
ko3luhbka <ko3luhbka@gmail.com>
2019-11-21 23:24:00 +0300

    Improve messages & errors flashing

--- todo_list/views.py
+++ todo_list/views.py
@@ -2,37 +2,40 @@ import logging
 
 from flask import flash, render_template, request
 
-from todo_list import app, db_utils
+from todo_list import app
+from todo_list.db_utils import QueryDB
 from todo_list.forms import AddToDoTaskForm, UpdateStatusForm
 from todo_list.todo_task import TaskStatus
+
+db = QueryDB()
 
 
 @app.route("/tasks/all")
 def show_all_tasks():
     """Show all todo list tasks"""
-
-    res_data = db_utils.get_all_tasks()
+    query_result = db.get_all_tasks()
 
     return render_template(
         "all_tasks.html",
-        tasks=res_data["tasks"]
+        tasks=query_result["tasks"],
     )
 
 
 @app.route("/tasks/new", methods=["GET", "POST"])
 def add_new_task():
     """Add new todo list task"""
-
     form = AddToDoTaskForm()
     if form.validate_on_submit():
         task = form.task.data
-        db_utils.add_task(task)
-        flash(f"Task '{task}' was added successfuly")
+        query_result = db.add_task(task)
+        if isinstance(query_result, dict):
+            flash(f"Task '{task}' was added successfuly")
+        elif isinstance(query_result, str):
+            flash(query_result, category='error')
         return render_template(
             "all_tasks.html",
-            tasks=db_utils.get_all_tasks()["tasks"]
+            tasks=db.get_all_tasks()["tasks"],
         )
-
     logging.info(form.errors)
     return render_template(
         "add_task.html",
@@ -43,13 +46,12 @@ def add_new_task():
 @app.route("/tasks/update/<task_name>", methods=["GET", "POST"])
 def update_status(task_name):
     """Update todo list task status"""
-
-    current_status = db_utils.get_task(task_name)
+    current_status = db.get_task(task_name)
     form = UpdateStatusForm(status=TaskStatus(current_status).name)
 
     if request.method == "GET":
         logging.info(
-            "current status of task [%s]: [%s]",
+            "Current status of task [%s]: [%s]",
             task_name,
             current_status,
         )
@@ -63,17 +65,17 @@ def update_status(task_name):
         new_status = TaskStatus[form.status.data].value
         if new_status == current_status:
             logging.info(
-                "status of task [%s] didnt change: [%s]",
+                "Status of task [%s] didnt change: [%s]",
                 task_name,
                 current_status,
             )
         else:
             logging.info(
-                "new status of task [%s]: [%s]",
+                "New status of task [%s]: [%s]",
                 task_name,
                 new_status,
             )
-            db_utils.update_status(
+            db.update_status(
                 task_name,
                 new_status,
             )
@@ -81,7 +83,7 @@ def update_status(task_name):
 
         return render_template(
             "all_tasks.html",
-            tasks=db_utils.get_all_tasks()["tasks"],
+            tasks=db.get_all_tasks()["tasks"],
         )
 
 
@@ -89,10 +91,10 @@ def update_status(task_name):
 def delete_task(task_name):
     """Delete todo list task"""
 
-    db_utils.delete_task(task_name)
+    db.delete_task(task_name)
     flash(f"Task '{task_name}' was deleted successfuly")
 
     return render_template(
         "all_tasks.html",
-        tasks=db_utils.get_all_tasks()["tasks"]
+        tasks=db.get_all_tasks()["tasks"]
     )
