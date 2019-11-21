39b629b643cbd51eaf38662b612d4e5e0d447b5e
ko3luhbka <ko3luhbka@gmail.com>
2019-11-21 23:21:35 +0300

    Move all DB-related functions into QueryDB class

--- todo_list/db_utils.py
+++ todo_list/db_utils.py
@@ -1,100 +1,86 @@
 import logging
 import os
 import sqlite3
+from typing import Optional, Union
 
 from todo_list.todo_task import TaskStatus
 
-DB_PATH = os.path.join(
-    os.path.dirname(__file__), 
-    "todo.db",
-)
 
+class QueryDB:
+    """Class contains SQLite database query methods."""
 
-def connect_to_db(db_path=DB_PATH):
-    conn = sqlite3.connect(db_path)
-    return conn
+    def __init__(self):
+        self.db_path = os.path.join(os.path.dirname(__file__), "todo.db")
+        try:
+            self.db_connection = self._connect_to_db()
+            self.db_cursor = self.db_connection.cursor()
+        except sqlite3.DatabaseError:
+            logging.exception("Database connection error")
 
+    def add_task(self, task: str) -> Union[dict, str]:
+        """Add new todo list task."""
+        logging.info("Adding task [%s]", task)
+        try:
+            self.db_cursor.execute(
+                "INSERT INTO tasks(task, status) VALUES(?,?)",
+                (task, TaskStatus.not_started.value),
+            )
+            self.db_connection.commit()
+            return {"task": task, "status": TaskStatus.not_started.value}
+        except sqlite3.IntegrityError:
+            err_msg = "Error: task '{0}' already exists!".format(task)
+            logging.exception(err_msg)
+            return err_msg
 
-def add_task(task):
-    """Add new todo list task"""
+    def get_all_tasks(self) -> Optional[dict]:
+        """Get all todo list tasks."""
+        logging.info("Getting all tasks")
+        try:
+            self.db_cursor.execute("SELECT * FROM tasks")
+            rows = self.db_cursor.fetchall()
+            return {"count: ": len(rows), "tasks": rows}
+        except sqlite3.DatabaseError:
+            logging.exception("Error while getting all tasks")
+            return None
 
-    logging.info("adding task [%s]", task)
-    try:
-        conn = connect_to_db()
-        cur = conn.cursor()
-        cur.execute(
-            "INSERT INTO tasks(task, status) VALUES(?,?)",
-            (task, TaskStatus.not_started.value),
-        )
-        conn.commit()
-        return {"task": task, "status": TaskStatus.not_started.value}
+    def get_task(self, task: str) -> Optional[dict]:
+        """Get one todo list task by name."""
+        logging.info("Getting task [%s]", task)
+        try:
+            self.db_cursor.execute(
+                f"SELECT status FROM tasks WHERE task='{task}'"
+            )
+            return self.db_cursor.fetchone()[0]
+        except sqlite3.DatabaseError:
+            logging.exception("Error while getting task {0}", task)
+            return None
 
-    except Exception as err:
-        logging.error("Error : %s", err)
-        return None
+    def update_status(self, task: str, status: str) -> Optional[dict]:
+        """Update todo list task status."""
+        logging.info("Updating status of task [%s] to [%s]", task, status)
+        try:
+            self.db_cursor.execute(
+                "UPDATE tasks SET status=? WHERE task=?",
+                (status, task),
+            )
+            self.db_connection.commit()
+            return {task: status}
+        except sqlite3.DatabaseError:
+            logging.exception("Error while updating status of task '{0}'", task)
+            return None
 
+    def delete_task(self, task: str) -> Optional[dict]:
+        """Delete task by name."""
+        logging.info("Deleting task [%s]", task)
+        try:
+            self.db_cursor.execute("DELETE FROM tasks WHERE task=?", (task,))
+            self.db_connection.commit()
+            return {"task": task}
+        except sqlite3.DatabaseError:
+            logging.exception("Error while deleting task '{0}'", task)
+            return None
 
-def get_all_tasks():
-    """Get all todo list tasks"""
-
-    logging.info("getting all tasks")
-    try:
-        conn = connect_to_db()
-        cur = conn.cursor()
-        cur.execute("select * from tasks")
-        rows = cur.fetchall()
-        return {"count: ": len(rows), "tasks": rows}
-
-    except Exception as err:
-        logging.error("Error : %s", err)
-        return None
-
-
-def get_task(task):
-    """Get one todo list task by name"""
-
-    logging.info("getting task [%s]", task)
-    try:
-        conn = connect_to_db()
-        cur = conn.cursor()
-        cur.execute(f"select status from tasks where task='{task}'")
-        status = cur.fetchone()[0]
-        return status
-    except Exception as err:
-        logging.error("Error : %s", err)
-        return None
-
-
-def update_status(task, status):
-    """Update todo list task status"""
-
-    logging.info(
-        "updating status of task [%s] to [%s]",
-        task,
-        status,
-    )
-    try:
-        conn = connect_to_db()
-        cur = conn.cursor()
-        cur.execute("update tasks set status=? where task=?", (status, task))
-        conn.commit()
-        return {task: status}
-    
-    except Exception as err:
-        logging.error("Error: %s", err)
-        return None
-
-
-def delete_task(task):
-    """Delete task by name"""
-
-    logging.info("deleting task [%s]", task)
-    try:
-        conn = connect_to_db()
-        cur = conn.cursor()
-        cur.execute("delete from tasks where task=?", (task,))
-        conn.commit()
-        return {"task": task}
-    except Exception as err:
-        logging.error("Error: %s", err)
-        return None
+    def _connect_to_db(self):
+        # It's safe to set `check_same_thread` argument to False because
+        # we work in single thread
+        return sqlite3.connect(self.db_path, check_same_thread=False)
