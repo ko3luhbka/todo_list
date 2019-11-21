0433425c584d82e3441ea3a8a4c9ba6a3dfc54c4
ko3luhbka <ko3luhbka@gmail.com>
2019-11-21 23:22:34 +0300

    Minor style changes

--- todo_list/forms.py
+++ todo_list/forms.py
@@ -4,21 +4,24 @@ from wtforms.validators import DataRequired, Length
 
 from todo_list.todo_task import TaskStatus
 
+MIN_TASK_NAME_LENGTH = 1
+MAX_TASK_NAME_LENGTH = 20
+
 
 class AddToDoTaskForm(FlaskForm):
     """Form for adding new todo task"""
 
-    task = StringField(
-        label="Task:",
-        validators=[
-            DataRequired(message="This field is required!"),
-            Length(
-                min=1,
-                max=20,
-                message="The field length should be from 1 to 20",
-            )
-        ]
-    )
+    task = StringField(label="Task:", validators=[
+        DataRequired(message="This field is required!"),
+        Length(
+            min=MIN_TASK_NAME_LENGTH,
+            max=MAX_TASK_NAME_LENGTH,
+            message="The field length should be from {} to {}".format(
+                MIN_TASK_NAME_LENGTH,
+                MAX_TASK_NAME_LENGTH,
+            ),
+        ),
+    ])
 
 
 class UpdateStatusForm(FlaskForm):
@@ -26,7 +29,5 @@ class UpdateStatusForm(FlaskForm):
 
     status = SelectField(
         label="Status: ",
-        choices=[
-            (status.name, status.value) for status in TaskStatus
-        ],
+        choices=[(status.name, status.value) for status in TaskStatus],
     )
