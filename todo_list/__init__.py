e29b34f0d6cd8ffbd1c7cef7bf5f27134133c003
ko3luhbka <ko3luhbka@gmail.com>
2019-12-01 15:05:36 +0300

    Use app factory for application initializing

--- todo_list/__init__.py
+++ todo_list/__init__.py
@@ -10,10 +10,27 @@ logging.basicConfig(
     level=logging.DEBUG,
 )
 
-app = Flask(__name__)
-# Workaround of "A secret key is required to use CSRF"error
-SECRET_KEY = os.urandom(32)
-TEMPLATES_AUTO_RELOAD = True
-app.config.from_object(__name__)
-app.add_template_global(generate_timestamp, name='timestamp')
-from todo_list import api, views
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY = 'dev',
+        DATABASE = os.path.join(app.instance_path, 'todo.db'),
+        TEMPLATES_AUTO_RELOAD = True
+    )
+    app.add_template_global(generate_timestamp, name='timestamp')
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    with app.app_context():
+        
+        from todo_list import api, views
+
+    return app
